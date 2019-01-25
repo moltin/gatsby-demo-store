6f07063bfbfc63087aa93342cb870bcb8b8cb994
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-01-25 12:12:33 +0000

    chore: Add gatsby-source-moltin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +27,12 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-moltin`,
+      options: {
+        client_id: process.env.MOLTIN_CLIENT_ID,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
