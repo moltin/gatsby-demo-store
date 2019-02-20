8636ca550aee955c682f748b886467041b9ba8f5
Jamie Barton <hi@jamiebarton.co.uk>
2019-02-20 20:36:36 +0000

    use correct imports

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import Layout from '../components/Layout/Layout'
+import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import ProductGrid from '../components/ProductGrid'
 
