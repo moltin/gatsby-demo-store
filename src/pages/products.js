b84527eb712bd44e25a8d67b80a477736c2c8b3a
Jamie Barton <hi@jamiebarton.co.uk>
2019-02-21 18:26:10 +0000

    fix file casing import

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import SEO from '../components/seo'
+import SEO from '../components/SEO'
 import ProductGrid from '../components/ProductGrid'
 
 const IndexPage = ({ data: { allMoltinProduct } }) => (
