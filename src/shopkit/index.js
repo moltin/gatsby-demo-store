93c49919670c52f9235e1d3f83a018a411df3f25
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-07-03 18:04:39 +0100

    feat: Add localStorage adapter for @moltin/request
    Abstract this to another package

--- src/shopkit/index.js
+++ src/shopkit/index.js
@@ -5,6 +5,20 @@ import { CartProvider, Cartkit } from './Cartkit'
 import { CustomerProvider, Customerkit } from './Customerkit'
 import { CheckoutProvider, Checkoutkit } from './Checkoutkit'
 
+class MoltinNodeStorageAdapter {
+  set(key, value) {
+    return window.localStorage.setItem(key, value)
+  }
+
+  get(key) {
+    return window.localStorage.getItem(key)
+  }
+
+  delete(key) {
+    return window.localStorage.removeItem(key)
+  }
+}
+
 let ShopkitContext
 
 const { Provider, Consumer } = (ShopkitContext = createContext())
@@ -18,7 +32,8 @@ function ShopkitProvider({
 }) {
   const moltin = new MoltinClient({
     client_id: clientId,
-    application: 'react-cartkit'
+    application: 'react-cartkit',
+    storage: new MoltinNodeStorageAdapter()
   })
 
   return (
