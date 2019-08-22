a20ea7fc6727174dbae1e0b152f4c62779e510e8
Sam Blacklock <samblacklock@me.com>
2019-08-22 11:22:15 +0100

    refactor: use classnames for pagination link

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,34 +1,34 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import cx from 'classnames'
 
-const NavLink = ({ url, text }) => (
-  <Link
-    to={url}
-    activeClassName="font-bold"
-    className={`${
-      !url ? 'text-grey-lighter pointer-events-none' : ''
-    } no-underline mx-4`}
-  >
-    {text}
-  </Link>
-)
+const PaginationLink = ({ url, text }) => {
+  const paginationLinkClass = cx('no-underline mx-4', {
+    'text-grey-lighter pointer-events-none': !url
+  })
+
+  return (
+    <Link to={url} activeClassName="font-bold" className={paginationLinkClass}>
+      {text}
+    </Link>
+  )
+}
 
 export default function Pagination({
-  currentPage,
   numberOfPages,
   nextPagePath,
   previousPagePath
 }) {
   return (
     <div className="flex justify-center items-center my-2">
-      <NavLink url={previousPagePath} text="‹‹" />
+      <PaginationLink url={previousPagePath} text="‹‹" />
       {Array.from({ length: numberOfPages }).map((_, i) => {
         const index = i + 1
         const url = i < 1 ? '/products' : `/products/${index}`
 
-        return <NavLink key={index} url={url} text={index} />
+        return <PaginationLink key={index} url={url} text={index} />
       })}
-      <NavLink url={nextPagePath} text="››" />
+      <PaginationLink url={nextPagePath} text="››" />
     </div>
   )
 }
