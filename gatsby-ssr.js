3d05065289e39f5b9d9bc0a4d9dcbeb9e8f9fce0
Rostyk <rkanafotskyy@gmail.com>
2020-04-15 14:15:18 +0300

    Added shopkit

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,6 +6,18 @@ import LayoutProvider from './src/components/LayoutProvider'
 import StripeProvider from './src/components/StripeProvider'
 import Layout from './src/components/Layout'
 
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  return setPostBodyComponents([
+    <script
+      src="https://unpkg.com/@bounteous/shopkit@0.0.1/dist/index.js"
+      id="shopkit-js"
+      key="moltin-shopkit"
+      data-moltin-client-id={process.env.GATSBY_MOLTIN_CLIENT_ID}
+      data-moltin-stripe-publishable-key={process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}
+    />
+  ]);
+}
+
 export const wrapRootElement = ({ element }) => {
   return (
     <StripeProvider>
--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from 'react'
 
 import { CartContext } from '../context'
-import Select from '../components/Select'
 
 export default function AddToCart({ disabled, productId }) {
   const { addToCart } = useContext(CartContext)
@@ -17,23 +16,10 @@ export default function AddToCart({ disabled, productId }) {
 
   return (
     <div className="inline-flex my-2">
-      <Select
-        options={new Array(10)
-          .fill(0)
-          .map((v, k) => k + 1)
-          .map(i => ({ id: i, name: i }))}
-        defaultValue={1}
-        onChange={updateQuantity}
-        disabled={disabled}
-      />
-
-      <button
-        onClick={handleAddToCart}
-        className="inline-block appearance-none bg-black border border-black text-white px-4 py-3 leading-tight rounded-none focus:outline-none ml-2"
-        disabled={disabled}
-      >
-        Add to Cart
-      </button>
+      <span
+        className="moltin-buy-button sk-button inline-block appearance-none bg-black border border-black text-white py-1 leading-tight rounded-none focus:outline-none"
+        data-moltin-product-id={productId}
+      ></span>
     </div>
   )
 }
